2500dfe0fbb75c335e67795be6a4c0016f952cc7
vrde <agranzot@mailbox.org>
2022-03-11 01:06:25 +0100

    Fix config for building ¯\_(ツ)_/¯

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,29 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import inject from "@rollup/plugin-inject";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  // Node.js global to browser globalThis
+  define: {
+    global: "globalThis",
+  },
   plugins: [
     svelte(),
     inject({
       util: "util/",
     }),
   ],
+  build: {
+    rollupOptions: {
+      plugins: [nodePolyfills()],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
